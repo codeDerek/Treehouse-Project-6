a07a9ef14c0fc8283f2aa08f85ab6c3fcf5381d1
Derek Daisey <dmdaisey@yahoo.com>
2019-03-21 22:43:17 -0400

    Comment out addEventlistener. Some pieces are broken and not working as intended. Need to

--- js/main.js
+++ js/main.js
@@ -1,16 +1,17 @@
 const qwerty = document.getElementById('qwerty');
+const keys = document.getElementById('.keyrow');
 const phrase = document.getElementById('phrase');
 const missed = 0;
+const startButton = document.querySelector('.btn__reset');
+// let buttonPressed = event.key;
+
 //For the getRandomPhraseAsArray function
 var characters = [];
-const startButton = document.querySelector('.btn__reset');
-
-
 
 //Phrases for game
 const phrases = [`Zebras Play Xylophones`, `George Washington had bad teeth`, `I love to code`, `Front End Web Developer for Hire`, `Disney World is VERY expensive`];
 
-
+//Functions
 
 function getRandomPhraseAsArray(array) {
   //Picks random phrase from the phrases array
@@ -23,31 +24,31 @@ function getRandomPhraseAsArray(array) {
 function addPhraseToDisplay(characters) {
   for (let i = 0; i < characters.length; i += 1) {
     const character = characters[i];
-  const ul = document.getElementById('phrase');
-  const li = document.createElement('li');
-  li.appendChild(document.createTextNode(character));
-//if the li is a space, do nothing, otherwise add class "letter"
-if (character === ' ')
-{} else {
-  li.classList.add('letter');
-  ul.appendChild(li);
-}
+    const ul = document.getElementById('phrase');
+    const li = document.createElement('li');
+    li.appendChild(document.createTextNode(character));
+    //if the li is a space, do nothing, otherwise add class "letter"
+    if (character === ' ') {} else {
+      li.classList.add('letter');
+      ul.appendChild(li);
+    }
   };
 }
 
+//When player presses button, check to see if letter matches letters. If they do, add the class 'show', return that letter. Otherwise, return null.
 function checkLetter(buttonPressed) {
-for (let i = 0; i < characters.length; i += 1) {
-const letters = document.getElementById('.letter')
-  if (buttonPressed === letters[i]) {
-    letters[i].classList.add('show');
-  let userLetter =  buttonPressed;
-  return userLetter;
-} else {
-  return null
-}
-}
+  for (let i = 0; i < characters.length; i += 1) {
+    const letters = document.getElementById('.letter')
+    if (buttonPressed === letters[i]) {
+      letters[i].classList.add('show');
+    } else {
+      return null
+    }
+  }
 }
 
+
+//*************Start of Program***********//
 
 //When the start button is clicked, the overlay dissappears revealing the game
 startButton.addEventListener('click', () => {
@@ -56,3 +57,12 @@ startButton.addEventListener('click', () => {
   getRandomPhraseAsArray(phrases);
   addPhraseToDisplay(characters);
 });
+
+// Use event delegation to listen only to button events from the keyboard. When a player chooses a letter, add the “chosen” class to that button so the same letter can’t be chosen twice.
+
+
+//The code below isn't returning correct.
+// qwerty.addEventListener('click', (event) => {
+// event.target.classList.add('chosen');
+// checkLetter(event);
+// });
